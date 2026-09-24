4287d08abde6b4ef773370b0ffecc0344a527a9d
Joaolpridolficarvalho <jlprcarvalho@gmail.com>
2026-09-24 15:00:12 -0300

    Adiciona: suporte a pacotes

--- interpretador-web.ts
+++ interpretador-web.ts
@@ -4,7 +4,7 @@ import { DeleguaModulo } from "@designliquido/delegua/interpretador/estruturas";
 import { ErroEmTempoDeExecucao } from "@designliquido/delegua/excecoes";
 
 export class InterpretadorWeb 
-    extends InterpretadorPitugues
+extends InterpretadorPitugues
 {
     constructor(
         diretorioBase: string,
@@ -14,7 +14,7 @@ export class InterpretadorWeb
     ) {
         super(diretorioBase, performance, funcaoDeRetorno ?? undefined, funcaoDeRetornoMesmaLinha ?? undefined);
     }
-
+    
     protected async logicaComumImportar(caminho: Literal, linha: number): Promise<DeleguaModulo> {
         switch (caminho.valor) {
             case 'criptografia':
@@ -23,25 +23,47 @@ export class InterpretadorWeb
             case 'json':
             case 'matematica':
             case 'tempo':
-                const variavelDoModulo = this.pilhaEscoposExecucao.obterVariavelPorNome(caminho.valor);
-                const moduloResolvido = variavelDoModulo.valor as DeleguaModulo;
-                return Promise.resolve(moduloResolvido);
+            const variavelDoModulo = this.pilhaEscoposExecucao.obterVariavelPorNome(caminho.valor);
+            const moduloResolvido = variavelDoModulo.valor as DeleguaModulo;
+            return Promise.resolve(moduloResolvido);
             default:
-                throw new ErroEmTempoDeExecucao(
-                    {
-                        hashArquivo: -1,
-                        linha: linha,
-                    } as SimboloInterface,
-                    `Biblioteca ${caminho.valor} não está disponível neste módulo Web. Para suporte a mais bibliotecas, por favor verifique a solução completa, em https://github.com/DesignLiquido/delegua-completo.`,
-                    linha
-                );
+            throw new ErroEmTempoDeExecucao(
+                {
+                    hashArquivo: -1,
+                    linha: linha,
+                } as SimboloInterface,
+                `Biblioteca ${caminho.valor} não está disponível neste módulo Web. Para suporte a mais bibliotecas, por favor verifique a solução completa, em https://github.com/DesignLiquido/delegua-completo.`,
+                linha
+            );
+        }
+    }
+    
+
+    private vincularElementosImportacao(declaracao: Importar, modulo: DeleguaModulo): void {
+        if (declaracao.simboloTudo !== null) {
+            this.pilhaEscoposExecucao.definirVariavel(declaracao.simboloTudo.lexema, modulo);
+            return;
+        }
+
+        for (const elemento of declaracao.elementosImportacao) {
+            const componente = modulo.componentes[elemento.lexema];
+            if (componente !== undefined) {
+                this.pilhaEscoposExecucao.definirVariavel(elemento.lexema, componente);
+            }
         }
     }
 
     override async visitarDeclaracaoImportar(declaracao: Importar): Promise<DeleguaModulo> {
         // TODO: Resolver isso não considerando que é um Literal.
         const caminhoResolvido = declaracao.caminho as Literal;
-        return this.logicaComumImportar(caminhoResolvido, declaracao.linha);
+        if (caminhoResolvido.valor === 'testes') {
+            // Reutiliza a implementação nativa do núcleo (registro, módulo e vínculo dos nomes).
+            return super.visitarDeclaracaoImportar(declaracao);
+        }
+
+        const modulo = await this.logicaComumImportar(caminhoResolvido, declaracao.linha);
+        this.vincularElementosImportacao(declaracao, modulo);
+        return modulo;
     }
 
     override async visitarExpressaoImportar(expressao: ImportarComoConstruto): Promise<DeleguaModulo> {
